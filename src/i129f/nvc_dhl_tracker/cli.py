87a92f0b0f92164b3ab47deceae70f7b10c72468
Collin Sage <3229549+MrSage@users.noreply.github.com>
2023-11-21 21:59:16 -0300

    feat: Work on the CLI interface.

--- src/i129f/nvc_dhl_tracker/cli.py
+++ src/i129f/nvc_dhl_tracker/cli.py
@@ -1,2 +1,14 @@
+import argparse
+
+
 def run_from_cli():
-    ...
+    # TODO finish this
+    parser = argparse.ArgumentParser(
+        prog="i129f_dhl_tracker",
+        description="Automatically track NVC shipments through DHL",
+        epilog="The wait is long, but we know how long",
+    )
+
+    parser.add_argument("filename")  # positional argument
+    parser.add_argument("-c", "--count")  # option that takes a value
+    parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
