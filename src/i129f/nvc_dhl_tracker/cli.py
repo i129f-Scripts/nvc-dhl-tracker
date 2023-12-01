fdcc810bf2c434170f589365f3e0071efb1dfc26
Collin Sage <3229549+MrSage@users.noreply.github.com>
2023-12-01 16:11:23 -0300

    feat: Setup CLI interface.

--- src/i129f/nvc_dhl_tracker/cli.py
+++ src/i129f/nvc_dhl_tracker/cli.py
@@ -1,15 +1,10 @@
-import argparse
+import django
+from coverage.annotate import os
+from django.core.management import call_command
 
 
 def run_from_cli():
-    # TODO finish this
-    parser = argparse.ArgumentParser(
-        prog="i129f_dhl_tracker",
-        description="Automatically track NVC shipments through DHL",
-        epilog="The wait is long, but we know how long",
-    )
-
-    parser.add_argument("filename")  # positional argument
-    parser.add_argument("-c", "--count")  # option that takes a value
-    parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
-    parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "i129f.nvc_dhl_tracker.settings")
+    django.setup()
+    call_command("migrate")
+    call_command("run_nvc_tracker")
